ba7821018eab02b15c9a602778b9e79ffeb6eee1
clemencecochard <clemence.cochard@ens-paris-saclay.fr>
2025-12-10 17:42:49 +0100

    heatmaps to test

--- src/network_utils.jl
+++ src/network_utils.jl
@@ -9,9 +9,7 @@ using Plots
 
 SNN.@load_units
 
-import SpikingNeuralNetworks: Population, Stimulus, SpikingSynapse, compose, monitor!, @update
-
-export build_network, plot_firing_rates, plot_membrane_potentials
+import SpikingNeuralNetworks: Population, Stimulus, SpikingSynapse, compose, monitor!, sim!, @update
 
 function build_network(config)
     @unpack seed = config
@@ -70,10 +68,10 @@ function plot_firing_rates(model;
         dt = 20ms,
         τ = 25ms,
         T = 3s,
-        name = "")
+        name = "",
+        colors = (:darkorange, :darkgreen, :purple, :darkcyan, :darkblue))
 
     time_axis = 0ms:dt:T #time axis
-    colors = (:darkorange, :darkgreen, :purple, :darkcyan, :darkblue)
     rates = Dict{Symbol, Vector{Float64}}()
     t = nothing
 
@@ -103,15 +101,21 @@ function plot_membrane_potentials(model;
         pops = (:TE, :CE, :PV, :SST, :VIP),
         neurons = 1:5,
         legend = false,
-        name = "")
+        name = "",
+        colors = (:darkorange, :darkgreen, :purple, :darkcyan, :darkblue))
 
     plt = plot(layout=(length(pops),1), 
                size=(600, 150*length(pops)), 
                legend=legend,
                bottom_margin=-2Plots.mm,
-               top_margin=-2Plots.mm)
+               top_margin=-2Plots.mm,
+               ylabel="V (mV)",
+               titlefontsize=11,
+               xguidefontsize=8,
+               yguidefontsize=8,
+               xtickfontsize=7,
+               ytickfontsize=7)
     
-    colors = (:darkorange, :darkgreen, :purple, :darkcyan, :darkblue)
     for (i,p) in enumerate(pops)
 
         vp = SNN.vecplot(model.pop[p], :v, neurons=neurons, add_spikes=true)
@@ -119,33 +123,112 @@ function plot_membrane_potentials(model;
         for s in vp.series_list
             xs = s[:x]
             ys = s[:y]
-            plot!(plt[i], xs, ys, lw=1.5, c=colors[i])
+            plot!(plt[i], xs, ys, lw=1.5, c=colors[i], label=String(p), legend = i)
         end
 
-        if i == length(pops)
+        if i == 1
             plot!(plt[i], 
-                  title="$name membrane potentials $p",
-                  xlabel="Time (s)",
-                  ylabel="V (mV)",
-                  titlefontsize=9,
-                  xguidefontsize=8,
-                  yguidefontsize=8,
-                  xtickfontsize=7,
-                  ytickfontsize=7)
+                  title="$name membrane potentials",
+                  xaxis=false)
+        elseif i == length(pops)
+            plot!(plt[i],
+                  xlabel="Time (s)")
         else
-            plot!(plt[i], 
-                  title="$name membrane potentials $p",
-                  ylabel="V (mV)",
-                  titlefontsize=9,
-                  xguidefontsize=8,
-                  yguidefontsize=8,
-                  xtickfontsize=7,
-                  ytickfontsize=7,
+            plot!(plt[i],
                   xaxis=false)
         end
     end
 
     return plt
 end
+
+function analysis(model; name = "Baseline", figs=true, save_figs=true, csv=false, μ=nothing, p=nothing)
+    
+    colors = (:darkorange, :darkgreen, :purple, :darkcyan, :darkblue)
+
+    if figs
+        # Raster plot
+        rplt = SNN.raster(model.pop, every=1, title="$name raster plot")
+
+        zrplt = SNN.raster(model.pop, every = 1, title = "$name raster plot zoomed")
+        xlims!(zrplt, 2, 2.5) # Zoom x-axis
+        ylims!(zrplt, 3500, 5200) # Zoom y-axis
+
+        # Firing rate dynamics
+        frplt = NetworkUtils.plot_firing_rates(model, name = name, colors = colors)
+
+        # Membrane potential dynamics 
+        vplt = NetworkUtils.plot_membrane_potentials(model, neurons = 1, name = name, colors = colors)
+        
+        if save_figs
+            savefig(zrplt, "$img_path/$name raster_zoom.png")
+            savefig(frplt, "$img_path/$name firing_rates.png")
+            savefig(rplt, "$img_path/$name raster_full.png")
+            savefig(vplt, "$img_path/$name membrane_potentials_dynamic.png")
+        end
+    end
+
+    # STTC
+    myspikes = SNN.spiketimes(model.pop)    
+    sttc_value = mean(STTC(myspikes[1:5:end]  , 50ms)) # Using subsampled myspikes: only 1 every 5
+
+    if csv
+        csvfile = "$img_path/$name sttc_results.csv"
+
+        open(csvfile, "a") do io
+            write(io, string(μ, ",", p, ",", sttc_value, "\n"))
+        end
+
+    else
+        open("$img_path/$name sttc_value.txt", "w") do io
+            write(io, string(sttc_value))
+        end
+    end
+    if figs
+        return rplt, zrplt, frplt, vplt
+    else
+        return nothing, nothing, nothing, nothing
+    end
+end
+
+function sttc_timeseries(model; window = 50ms, step = 10ms, sim_time = 3.0)
+    myspikes = SNN.spiketimes(model.pop)
+    t = 0:step:sim_time-window
+    sttc = Float64[]
+    for ti in t
+        spikes_window = map(myspikes) do s
+            s[(s .≥ ti) .& (s .< ti + window)]
+        end
+        push!(sttc, mean(STTC(spikes_window, window)))
+    end
+    return t, sttc
+end
+
+function transition_time(t, sttc; high = 0.8, sim_time = 3.0)
+    idx = findfirst(x -> x ≥ high, sttc)
+    return idx === nothing ? sim_time : t[idx]
+end
+
+function average_transition_time(network, pop, μ, p; seeds=1:10)
+    times = Float64[]
+    for s in seeds
+        Random.seed!(s)
+
+        modulation = (; network.connections[pop]..., μ = μ, p = p)
+        network_modified = (; network...,
+            connections = (; network.connections..., pop => modulation)
+        )
+        model = NetworkUtils.build_network(network_modified)
+        monitor!(model.pop, [:v], sr=1kHz)
+        sim!(model, 3s)
+        
+        t, sttc = sttc_timeseries(model)
+        tt = transition_time(t, sttc)
+        push!(times, tt)
+    end
+    return mean(times)
+end
+
+
 
 end
